fc0437e5713eaa6db48afaeaaf816f371b751c06
stepolimi <37816183+stepolimi@users.noreply.github.com>
2018-04-06 22:02:38 +0200

    test di funzionamento

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "adddddddioooo!" );
+        System.out.println( "adddddddioooo!(ehi_ciao_ci_sono_anche_io" );
     }
 }
