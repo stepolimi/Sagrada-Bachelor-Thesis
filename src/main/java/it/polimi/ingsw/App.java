02364635fd692068a4cdfdc73f352af96ccb17cc
stepolimi <37816183+stepolimi@users.noreply.github.com>
2018-04-06 22:14:05 +0200

    test di funzionamento con nuova classe

--- src/main/java/it/polimi/ingsw/App.java
+++ src/main/java/it/polimi/ingsw/App.java
@@ -1,5 +1,7 @@
 package it.polimi.ingsw;
 
+import static java.lang.System.*;
+
 /**
  * Hello world!
  *
@@ -9,5 +11,7 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "adddddddioooo!(ehi_ciao_ci_sono_anche_io_di_nuovo" );
+        classe_prova elemento=new classe_prova();
+        elemento.chiamata();
     }
 }
---
+++ src/main/java/it/polimi/ingsw/classe_prova.java
@@ -0,0 +1,7 @@
+package it.polimi.ingsw;
+
+public class classe_prova {
+    public void chiamata() {
+        System.out.println("funziona anche con altre classi");
+    }
+}
