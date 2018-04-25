5f11ffb58d312cc7637cde98d49bbf77d7fff769
stepolimi <37816183+stepolimi@users.noreply.github.com>
2018-04-25 12:08:59 +0200

    prova di testing

---
+++ src/test/java/it/polimi/ingsw/TestUnit.java
@@ -0,0 +1,16 @@
+package it.polimi.ingsw;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+
+
+public class TestUnit{
+
+    @Test
+    void myFirstTest(){
+        classe_prova i= new classe_prova();
+        assertTrue(i.chiamata());
+    }
+
+
+}
