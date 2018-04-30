6bb9e74f762e4edfa3f4a482c7ba957616975df7
bresc19 <matteo.bresciani@mal.polimi.it>
2018-04-30 17:56:29 +0200

    RoundTrack testing

---
+++ src/test/java/it/polimi/ingsw/RoundTrackTest.java
@@ -0,0 +1,41 @@
+package it.polimi.ingsw;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+
+public class RoundTrackTest {
+
+    RoundTrack trackround = new RoundTrack();
+    ArrayList<Dice> listDice = new ArrayList<Dice>();
+
+
+    public void setOfDice(){
+        Dice d1 = new Dice(Colour.ANSI_PURPLE, 3);
+        Dice d2 = new Dice(Colour.ANSI_GREEN, 2);
+        listDice.add(d1);
+        listDice.add(d2);
+
+    }
+
+    @Test
+    public void dice_insertion(){
+        setOfDice();
+        trackround.insertDice(listDice, 1);
+        assertEquals(listDice.size(), trackround.getListRounds(1).size());
+    }
+
+    @Test
+    public void dice_removing(){
+        setOfDice();
+        trackround.insertDice(listDice, 1);
+        listDice.add(trackround.removeDice(1,1));
+        assertEquals(listDice.size(), trackround.getListRounds(1).size());
+
+    }
+
+    
+}
