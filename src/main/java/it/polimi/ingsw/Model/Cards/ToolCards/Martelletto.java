bf734493c4bde282ba5f6e3db789c6783920163a
bresc19 <matteo.bresciani@mal.polimi.it>
2018-05-07 14:43:34 +0200

    modify toolcard

--- src/main/java/it/polimi/ingsw/Model/Cards/ToolCards/Martelletto.java
+++ src/main/java/it/polimi/ingsw/Model/Cards/ToolCards/Martelletto.java
@@ -1,6 +1,5 @@
 package it.polimi.ingsw.Model.Cards.ToolCards;
 
-import it.polimi.ingsw.Model.DiceSpace;
 import it.polimi.ingsw.Model.Player;
 import it.polimi.ingsw.Model.game.states.Round;
 
@@ -9,11 +8,11 @@ public class Martelletto extends ToolCard  {
     private static String description= "Tira nuovamente tutti i dadi della riserva. \n" +
             "Questa carta può essere utilizzata solo durante il tuo secondo turno, prima di scegliere il secondo dado";
     private static  int num_card = 7;
-    public boolean effects(Player p, Round round, DiceSpace diceSpace) {
+    public boolean effects(Player p, Round round) {
 
         if(isMySecondTurn(p, round) && round.getPendingDice() == null){
-            for(int i = 0; i < diceSpace.getListDice().size(); i++)
-                diceSpace.getListDice().get(i).rollDice();
+            for(int i = 0; i < round.getBoard().getDiceSpace().getListDice().size(); i++)
+                round.getBoard().getDiceSpace().getListDice().get(i).rollDice();
             return  true;
         }
         else return false;
