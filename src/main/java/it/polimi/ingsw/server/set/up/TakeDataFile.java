e00b6b9e32ac9b05f40227aa170d9c29723a0265
stepolimi <37816183+stepolimi@users.noreply.github.com>
2018-07-05 13:34:02 +0200

    few updates

--- src/main/java/it/polimi/ingsw/server/set/up/TakeDataFile.java
+++ src/main/java/it/polimi/ingsw/server/set/up/TakeDataFile.java
@@ -12,7 +12,6 @@ import java.util.logging.Level;
 import static it.polimi.ingsw.server.costants.LogConstants.*;
 
 public class TakeDataFile {
-    private FileReader reader;
     private String file;
     public TakeDataFile(String file)
     {
@@ -21,9 +20,7 @@ public class TakeDataFile {
 
     public String getParameter(String parameter)   {
         String result = null;
-        try (BufferedReader input = new BufferedReader(reader)){
-            reader = new FileReader(file);
-
+        try ( FileReader reader = new FileReader(file); BufferedReader input = new BufferedReader(reader)){
             Optional<String> optional =  input.lines()
                     .filter(line-> line.contains(parameter))
                     .map(line -> line.substring(parameter.length()+1))
@@ -32,20 +29,9 @@ public class TakeDataFile {
                 result = optional.get();
         } catch (IOException e) {
             Log.getLogger().addLog(CONFIGURATION_FILE_ERROR, Level.SEVERE,this.getClass().getName(),TAKE_DATA_FILE_GET_PARAMETER);
-        } finally {
-            closeBuffer();
         }
-
         return result;
     }
 
-    private void closeBuffer(){
-        try {
-            reader.close();
-        } catch (IOException e) {
-            Log.getLogger().addLog(e.getMessage(),Level.SEVERE,this.getClass().getName(),TAKE_DATA_FILE_CLOSE_BUFFER);
-        }
-
-    }
 }
 
